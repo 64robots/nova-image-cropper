e9c147c12547b191c63932f0f54ea4d8ff0fe969
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2018-09-02 10:56:22 +0300

    Optimize build size

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,18 @@
-let mix = require('laravel-mix')
+const mix = require('laravel-mix')
 
 mix.js('resources/js/field.js', 'dist/js')
    .sass('resources/sass/field.scss', 'dist/css')
+    .setPublicPath('./')
     .webpackConfig({
         resolve: {
             symlinks: false
         }
+    })
+    .babelConfig({
+        plugins: [
+            ['component', {
+                libraryName: 'element-ui',
+                styleLibraryName: 'theme-chalk'
+            }]
+        ]
     })
